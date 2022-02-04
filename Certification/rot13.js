901075807c4faba2313f6cd68df28813b63d205d
camilord-end <juliancaor10@hotmail.com>
2022-02-04 15:22:52 -0500

    simplified

--- Certification/rot13.js
+++ Certification/rot13.js
@@ -5,18 +5,20 @@ function rot13(str) {
     if (!(/[A-Z]/).test(str[0]) && str.length==1 ){     
         return str[0] 
     }
-    if ((/[A-Z]/).test(str[0]) && str.length>0 && str[0].charCodeAt(0)>77){
+    if (str.length>0 && str[0].charCodeAt()>77){
         return String.fromCharCode(str[0].charCodeAt(0)-13)+ rot13(str.slice(1));
     }
-    if ((/[A-Z]/).test(str[0]) && str.length==1 && str[0].charCodeAt(0)>77){
+    if (str.length==1 && str[0].charCodeAt()>77){
         return String.fromCharCode(str[0].charCodeAt(0)-13)
     }
-    if ((/[A-Z]/).test(str[0]) && str.length>0 && str[0].charCodeAt(0)<78){
+    if (str.length>0 && str[0].charCodeAt()<78){
         return String.fromCharCode(str[0].charCodeAt(0)+13)+ rot13(str.slice(1));
     }
-    if ((/[A-Z]/).test(str[0]) && str.length>0 && str[0].charCodeAt(0)<78){
+    if (str.length>0 && str[0].charCodeAt()<78){
         return String.fromCharCode(str[0].charCodeAt(0)+13)
     }
     return str
   }
+  console.log(rot13("SERR YBIR?"))
+  console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."))
  
