44c89719165086d2d83aa0b810c4ff0637366128
camilord-end <juliancaor10@hotmail.com>
2022-02-05 10:39:46 -0500

    TotalMoney checker working

--- Certification/cashRegister.js
+++ Certification/cashRegister.js
@@ -1,6 +1,18 @@
 function checkCashRegister(price, cash, cid) {
-    let change;
+    let change=cash-price;
+    if (cidTotalMoney(cid)<change){
+        return {status: "INSUFFICIENT_FUNDS", change: []}
+    }
     return change;
   }
-  
+  function cidTotalMoney(cid){
+      if (cid.length==1){
+          return cid[0][1];
+      }
+      if (cid.length>1){
+          return cid[0][1] + cidTotalMoney(cid.slice(1));
+      }
+  }
   checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  console.log(cidTotalMoney([["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]))
+  
