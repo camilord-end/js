0f5f16c63d16db02e41e9284f290bd500b04d4ca
camilord-end <jcorozcoa@correo.udistrital.edu.co>
2022-03-14 11:13:23 -0500

    little styling

--- basicJsGames/rockPaperScissors/app.js
+++ basicJsGames/rockPaperScissors/app.js
@@ -6,13 +6,16 @@ const possibleChoices = document.querySelectorAll("button");
 let userChoice = undefined;
 let computerChoice = undefined;
 let result = undefined;
+let bolIcon = undefined;
 
 possibleChoices.forEach((choices) =>
   choices.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
-    resultDisplay.innerHTML = getResult(computerChoice, userChoice);
+    resultDisplay.innerHTML = getResult(computerChoice, userChoice)
+      ? `<i id="icon-output-win" class="bi bi-shield-fill-check"> You win</i>`
+      : `<i id="icon-output-lose" class="bi bi-shield-fill-x"> You lose</i>`;
   })
 );
 
@@ -33,24 +36,31 @@ function generateComputerChoice() {
 function getResult(computerChoice, userChoice) {
   if (computerChoice === userChoice) {
     result = "draw";
+    bolIcon = undefined;
   }
   if (computerChoice === "rock" && userChoice === "paper") {
     result = "you win";
+    bolIcon = true;
   }
   if (computerChoice === "rock" && userChoice === "scissors") {
     result = "you lose";
+    bolIcon = false;
   }
   if (computerChoice === "paper" && userChoice === "scissors") {
     result = "you win";
+    bolIcon = true;
   }
   if (computerChoice === "paper" && userChoice === "rock") {
     result = "you lose";
+    bolIcon = true;
   }
   if (computerChoice === "scissors" && userChoice === "rock") {
     result = "you win";
+    bolIcon = true;
   }
   if (computerChoice === "scissors" && userChoice === "paper") {
     result = "you lose";
+    bolIcon = true;
   }
-  return result;
+  return bolIcon;
 }
